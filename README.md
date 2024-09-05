8ea8f613efe0e32b0dfb7d5f0d0a858a79668ae3
kasev <vojtech.kase@gmail.com>
2024-09-05 13:02:39 +0200

    viz with latest data works locally

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-#  [Repository title]
+#  tomeviz: interactive visualizations of latin embeddings
 
 ---
 ## Authors
-* [list of all contributors, with their institutional email and ORCID]
-
+* [see citation file]
 
 ## License
-CC-BY-SA 4.0, see attached License.md
+CC-BY-SA 4.0, see attached LICENSE.md
 
 ---
 ## Description
@@ -15,11 +14,26 @@ CC-BY-SA 4.0, see attached License.md
 
 ## Getting started
 
-```bash
+### To setup your python evnironment
+
+```
 git clone [url-of-the-git-file]
 cd [name-of-the-repo]
+python3 -m venv tomevizvenv
+source tomevizvenv/bin/activatate
 pip install -r requirements.txt
+ipykernel install --user --name tomeviz_kernel
 ```
+
+### To download the latest data
+```
+curl -L https://github.com/CCS-ZCU/noscemus_ETF/raw/master/data/vectors_dict_comp.pkl -o data/vectors_dict_comp_TEST.pkl
+curl -L https://github.com/CCS-ZCU/noscemus_ETF/raw/master/data/coordinates3d_dict.pkl -o data/coordinates3d_dict.pkl
+curl -L https://github.com/CCS-ZCU/noscemus_ETF/raw/master/data/filtered_vocab_df.json -o data/filtered_vocab_df.json
+curl -L https://github.com/CCS-ZCU/noscemus_ETF/raw/master/data/metadata_table_long.json -o data/metadata_table_long.json
+
+```
+
 
 Go to `scripts` and run the notebooks
 
--- tomeviz.py
+++ tomeviz.py
@@ -15,23 +15,25 @@ topn = 20
 # loading data
 # Load the dataset of positions of all words in 3D space
 path = "data/"
-with open(path + "coordinates3s_dict.pkl", "rb") as f:
-    coordinates3s_dict = pickle.load(f)#url = "https://raw.githubusercontent.com/CCS-ZCU/noscemus_ETF/master/data/coordinates3s_dict.pkl"
+with open(path + "coordinates3d_dict.pkl", "rb") as f:
+    coordinates3d_dict = pickle.load(f)
+#url = "https://raw.githubusercontent.com/CCS-ZCU/noscemus_ETF/master/data/coordinates3s_dict.pkl"
 #resp = requests.get(url)
 # coordinates3s_dict = pickle.loads(resp.content)
 
-filtered_vocab_df = pd.read_json(path + "filtered_vocab_df.json")
+filtered_vocab_df = pd.read_json(path + "filtered_vocab_df_v0-1.json")
 # filtered_vocab_df = pd.read_json("https://raw.githubusercontent.com/CCS-ZCU/noscemus_ETF/master/data/filtered_vocab_df.json")
+
 filtered_vocab_df.set_index("word", inplace=True)
 
-with open(path + "vectors_dict_comp.pkl", "rb") as f:
+with open(path + "vectors_dict_comp_v0-1.pkl", "rb") as f:
     vectors_dict = pickle.load(f)
 #url = "https://raw.githubusercontent.com/CCS-ZCU/noscemus_ETF/master/data/vectors_dict_comp.pkl"
 #resp = requests.get(url)
 #vectors_dict = pickle.loads(resp.content)
 
 # selecting data on the basis of subcorporpus & target
-xs, ys, zs, words = coordinates3s_dict[subcorpus]
+xs, ys, zs, words = coordinates3d_dict[subcorpus]
 word_dict = filtered_vocab_df.apply(lambda row: "wordcount: " + str(row[subcorpus]) + ", translation: " + row["transl"], axis=1).to_dict()
 
 wordlist = [target] + [tup[0] for tup in vectors_dict[subcorpus].most_similar(target, topn=topn)]
@@ -40,17 +42,24 @@ wordlist_xs, wordlist_ys, wordlist_zs = xs[idx], ys[idx], zs[idx]
 
 hover_text = [word + ": " + word_dict[word] for word in wordlist]
 
-
-
 app = Dash(__name__)
 server = app.server  # You can explicitly define server
 
 app.layout = html.Div([
-    html.H1(children='Interactive word embeddings', style={'textAlign': 'center'}),
+    html.H1(children='tomeviz: Interactive word embeddings for latin ', style={'textAlign': 'center'}),
     dcc.Dropdown(options=[{'label': key, 'value': key} for key in vectors_dict.keys()], value='1501-1550',
                  id='dropdown-selection'),
     dcc.Input(id="target-term", value="scientia", type="text"),
     html.Button(id='submit-button', n_clicks=0, children='Submit'),
+    dcc.Slider(
+        id='topn-slider',
+        min=5,
+        max=100,
+        step=1,
+        value=20,  # Default value
+        marks={i: str(i) for i in range(5, 101, 10)},  # Marks every 10 steps
+        tooltip={"placement": "bottom", "always_visible": True}
+    ),
     dcc.Graph(id='graph-content', style={'width': '100%', 'height': '100vh'})
 ])
 
@@ -59,13 +68,15 @@ app.layout = html.Div([
     Output('graph-content', 'figure'),
     [Input('submit-button', 'n_clicks'),
      State('dropdown-selection', 'value'),
-     State('target-term', 'value')]
+     State('target-term', 'value'),
+     State('topn-slider', 'value')]  # Add slider value as State
+
 )
-def update_graph(n_clicks, subcorpus, target):
+def update_graph(n_clicks, subcorpus, target, topn):
     if n_clicks < 1:  # If submit-button has not been clicked yet, do not update
         return dash.no_update
 
-    xs, ys, zs, words = coordinates3s_dict[subcorpus]
+    xs, ys, zs, words = coordinates3d_dict[subcorpus]
     word_dict = filtered_vocab_df.apply(
         lambda row: "wordcount: " + str(row[subcorpus]) + ", translation: " + row["transl"], axis=1).to_dict()
 
@@ -93,11 +104,11 @@ def update_graph(n_clicks, subcorpus, target):
     fig.update_layout(
         title='Embeddings',
         scene=dict(
-            xaxis=dict(title='', showgrid=False, showline=False, showticklabels=False, zeroline=False,
+            xaxis=dict(title='', showgrid=False, showline=False, showticklabels=False, zeroline=False, showbackground=False,
                        linecolor='rgba(0,0,0,0)'),
-            yaxis=dict(title='', showgrid=False, showline=False, showticklabels=False, zeroline=False,
+            yaxis=dict(title='', showgrid=False, showline=False, showticklabels=False, zeroline=False, showbackground=False,
                        linecolor='rgba(0,0,0,0)'),
-            zaxis=dict(title='', showgrid=False, showline=False, showticklabels=False, zeroline=False,
+            zaxis=dict(title='', showgrid=False, showline=False, showticklabels=False, zeroline=False, showbackground=False,
                        linecolor='rgba(0,0,0,0)'),
             bgcolor='rgba(255,255,255,0)'
         ),
