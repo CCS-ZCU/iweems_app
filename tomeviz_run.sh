85d6ff55e287eb98c1bee58e031bb584b312819f
tvrzj <tvrzj@ff.zcu.cz>
2024-12-12 20:54:13 +0100

    amend

--- tomeviz_run.sh
+++ tomeviz_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/tvrzj/Documents/Projects/tomeviz/
 source .env/bin/activate
-streamlit run totomeme.py --server.address localhost --server.port 8052 --browser.gatherUsageStats False
+streamlit run tomeviz-streamlit.py --server.address localhost --server.port 8052 --browser.gatherUsageStats False
